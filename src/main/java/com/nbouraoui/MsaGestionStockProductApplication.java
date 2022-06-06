24fc960a23e0ca8c88b17dab1836505788916553
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-06-06 09:51:10 +0100

    init project.

---
+++ src/main/java/com/nbouraoui/MsaGestionStockProductApplication.java
@@ -0,0 +1,13 @@
+package com.nbouraoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaGestionStockProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaGestionStockProductApplication.class, args);
+	}
+
+}
