f9e605a8f92a40fd2080738bf79a709c5d4911a3
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-06-09 07:36:50 +0100

    add MSA-gestion-stock-server.

---
+++ src/test/java/com/nbouraoui/MsaGestionStockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbouraoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaGestionStockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
