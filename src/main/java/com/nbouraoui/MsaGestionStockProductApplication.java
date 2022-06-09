48eb4e856bd881368994cda099083bb79ca69596
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-06-09 07:59:25 +0100

    prepare product to implement server.

--- src/main/java/com/nbouraoui/MsaGestionStockProductApplication.java
+++ src/main/java/com/nbouraoui/MsaGestionStockProductApplication.java
@@ -2,11 +2,13 @@ package com.nbouraoui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaClient
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsaGestionStockProductApplication {
 
 	public static void main(String[] args) {
